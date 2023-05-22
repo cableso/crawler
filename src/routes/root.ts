5688d4f459d2d96aca04ee77e780ba2e85d95e0c
1CAMOU <mgmt.camou@gmail.com>
2023-05-22 13:25:06 +0200

    ✨ sitemap crawler & page crawler with markdown format

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,9 +1,67 @@
-import { FastifyPluginAsync } from "fastify";
+import { FastifyPluginAsync, FastifyReply, FastifyRequest } from "fastify";
+import Crawler from "crawler";
+import TurndownService from "turndown";
+import axios from "axios";
+
+type CrawlRequest = FastifyRequest<{
+	Querystring: { url: string };
+}>;
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-	fastify.get("/", async function (request, reply) {
-		return { root: true };
-	});
+	const turndownService = new TurndownService();
+
+	fastify.get(
+		"/",
+		async function (request: CrawlRequest, reply: FastifyReply) {
+			const pages: string[] = [];
+			let urls: string[] = [];
+			const { url } = request.query;
+
+			const response = await axios.get(
+				process.env.BASE_URL + "/urls?url=" + url
+			);
+			urls = response.data;
+
+			const crawler = new Crawler({
+				rateLimit: 100,
+				callback: async (error, res, done) => {
+					if (error) {
+						console.log(error);
+					} else {
+						const $ = res.$;
+						const htmlContent = $("body").html();
+						const markdown = turndownService.turndown(
+							htmlContent || ""
+						);
+
+						pages.push(markdown);
+						console.log(
+							"Written " +
+								pages.length +
+								"/" +
+								urls.length +
+								" files"
+						);
+					}
+
+					done();
+				},
+			});
+
+			crawler.queue(urls);
+
+			await new Promise((resolve) => {
+				crawler.on("drain", function () {
+					resolve(true);
+				});
+			});
+
+			return reply.send({
+				message:
+					"Successfully crawled & saved " + urls.length + " pages",
+			});
+		}
+	);
 };
 
 export default root;
---
+++ src/routes/urls/index.ts
@@ -0,0 +1,81 @@
+import { FastifyPluginAsync, FastifyReply, FastifyRequest } from "fastify";
+import RobotsParser from "robots-parser";
+import axios from "axios";
+import cheerio from "cheerio";
+
+type CrawlRequest = FastifyRequest<{
+	Querystring: { url: string };
+}>;
+
+const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+	fastify.get(
+		"/",
+		async function (request: CrawlRequest, reply: FastifyReply) {
+			const { url } = request.query;
+
+			// Fetch and parse robots.txt
+			const robotsResponse = await axios.get(`${url}/robots.txt`);
+			const robotsTxt = robotsResponse.data;
+			const robots = RobotsParser(`${url}/robots.txt`, robotsTxt);
+
+			// Fetch all urls from all sitemaps
+			const urls: string[] = [];
+			await Promise.all(
+				robots.getSitemaps().map(async (sitemapIndex) => {
+					const response = await axios.get(sitemapIndex);
+
+					const $ = cheerio.load(response.data, {
+						xmlMode: true,
+					});
+
+					// First, check if it's a sitemap index
+					if ($("sitemapindex").length > 0) {
+						// It's a sitemap index, get individual sitemaps
+						const sitemaps: string[] = [];
+						$("sitemap loc").each(function (this: cheerio.Element) {
+							const sitemapUrl = $(this).text();
+							sitemaps.push(sitemapUrl);
+						});
+
+						// Fetch individual sitemaps
+						await Promise.all(
+							sitemaps.map(async (sitemapUrl) => {
+								const sitemapResponse = await axios.get(
+									sitemapUrl
+								);
+
+								const sitemap$ = cheerio.load(
+									sitemapResponse.data,
+									{
+										xmlMode: true,
+									}
+								);
+
+								sitemap$("url loc").each(function (
+									this: cheerio.Element
+								) {
+									const url = sitemap$(this).text();
+									if (robots.isAllowed(url)) {
+										urls.push(url);
+									}
+								});
+							})
+						);
+					} else {
+						// It's not a sitemap index, so fetch URLs directly
+						$("url loc").each(function (this: cheerio.Element) {
+							const url = $(this).text();
+							if (robots.isAllowed(url)) {
+								urls.push(url);
+							}
+						});
+					}
+				})
+			);
+
+			return urls;
+		}
+	);
+};
+
+export default root;
